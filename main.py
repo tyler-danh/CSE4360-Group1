90ec71dfdcb95704ad048bee72bc1d24e8296a5e
275RR-Public <105179611+275RR-Public@users.noreply.github.com>
2024-10-21 23:21:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor)
-from pybricks.tools import StopWatch, wait
+from pybricks.tools import StopWatch
 from pybricks.parameters import Port, Direction
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.media.ev3dev import ImageFile
 
-from controller import Controller
+#from controller import Controller
+from simple_controller import Controller
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
@@ -28,29 +29,25 @@ wheel_base = 110.7186       # AXEL(mm) from center of wheel to center of wheel
 wheel_radius = 34.7218      # RADIUS(mm) from (floor to top of wheel) / 2
 
 watch = StopWatch()
-watch2 = StopWatch()
 
 motorRight = Motor(Port.A, positive_direction=Direction.CLOCKWISE, gears=None)
 motorLeft= Motor(Port.D, positive_direction=Direction.CLOCKWISE, gears=None)
 
-control = Controller(wheel_base, wheel_radius, watch, watch2, wait, motorLeft, motorRight)
+#control = Controller(wheel_base, wheel_radius, watch, watch2, wait, motorLeft, motorRight)
+control = Controller(wheel_base, wheel_radius, watch, motorLeft, motorRight)
 
 ## CREATE PATH ##
-# list of waypoints [(x,y,angle), (x,y,angle), ...]
-# x,y in mm, angle in deg
+# list of waypoints [(dist,angle), (dist,angle), ...]
+# dist in mm, angle in deg
 # robot starts on x axis facing 0 deg
 # add 90 for left turn, subtract 90 for right turn
 
-# example path (a straight line)
-#path = [(450,0,0)]
+# example path (a square with left turns)
+#path = [(450, 90), (450,90), (450,90), (450,90)]
 
-# example path (a square)
-#path = [(450,0,90), (450,450,180), (0,450,270), (0,0,360)]
-
-# example path
-# 450 with left turn, 450 straight, 450 with right turn
-#path = [(450,0,90), (450,450,90), (450,450,0)]
-path = [(450,0,90), (450,450,180), (0,450,270), (0,0,0)]
+# example path (a square with left turns)
+#path = [(450, -90), (450,-90), (450,-90), (450,-90)]
+path = [(360, -90)]
 
 ## EXECUTE ##
 control.follow_path(path)
