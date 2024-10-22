c5ee8d95ed6869689d68a48670ec5aa4488260c6
275RR-Public <105179611+275RR-Public@users.noreply.github.com>
2024-10-22 00:13:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor)
 from pybricks.tools import StopWatch
 from pybricks.parameters import Port, Direction
-from pybricks.media.ev3dev import ImageFile
 import pathfinder3
 
 #from controller import Controller
@@ -16,15 +15,6 @@ from simple_controller import Controller
 ev3 = EV3Brick()
 ev3.speaker.beep()
 
-"""
-# Display a question mark to indicate robot waiting
-# Wait until any Brick Button is pressed.
-ev3.screen.load_image(ImageFile.QUESTION_MARK)
-while not any(ev3.buttons.pressed()):
-    wait(10)
-ev3.screen.clear()
-"""
-
 ## INIT CONTROLS ##
 wheel_base = 110.7186       # AXEL(mm) from center of wheel to center of wheel
 wheel_radius = 34.7218      # RADIUS(mm) from (floor to top of wheel) / 2
@@ -35,7 +25,7 @@ motorRight = Motor(Port.A, positive_direction=Direction.CLOCKWISE, gears=None)
 motorLeft= Motor(Port.D, positive_direction=Direction.CLOCKWISE, gears=None)
 
 #control = Controller(wheel_base, wheel_radius, watch, watch2, wait, motorLeft, motorRight)
-control = Controller(wheel_base, wheel_radius, watch, motorLeft, motorRight)
+simple_control = Controller(wheel_base, wheel_radius, watch, motorLeft, motorRight)
 
 ## CREATE PATH ##
 # list of waypoints [(dist,angle), (dist,angle), ...]
@@ -48,8 +38,7 @@ control = Controller(wheel_base, wheel_radius, watch, motorLeft, motorRight)
 
 # example path (a square with left turns)
 #path = [(450, -90), (450,-90), (450,-90), (450,-90)]
-path = [(360, -90)]
 
 ## EXECUTE ##
-control.follow_path(pathfinder3.search())
+simple_control.follow_path(pathfinder3.search())
 ev3.speaker.beep()
