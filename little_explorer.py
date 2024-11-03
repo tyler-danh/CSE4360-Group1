2ffbc1f15205edad5de27acc31fedfccee048e72
Daniel <105179611+275RR-Public@users.noreply.github.com>
2024-11-02 20:48:37 -0500

    Update little_explorer.py

--- little_explorer.py
+++ little_explorer.py
@@ -2,20 +2,33 @@ from pybricks.parameters import Color, Stop
 from pybricks.media.ev3dev import SoundFile
 from pybricks.ev3devices import Motor, UltrasonicSensor, GyroSensor, ColorSensor, TouchSensor
 from pybricks.tools import wait
+from enum import Enum, auto
+
+# states of robot
+# auto() handles int assignment automatically
+class State(Enum):
+    WANDERING = auto()
+    WALL_FOLLOWING = auto()
+
 class Explorer:
-    def __init__(self, ev3, left_motor, right_motor, gyroscope, ultrasonic, colorSensor, touch):
+    def __init__(self, ev3, left_motor, right_motor, gyroscope, ultrasonic, colorSensor, touch, watch):
         self.ev3 = ev3
         self.left_motor = left_motor
         self.right_motor = right_motor
         self.gyroscope = gyroscope
         self.supersonic = ultrasonic
         self.colorsensor = colorSensor
-        #self.ultrasonic2 = ultrasonic2
         self.touch = touch
+        self.stopwatch = watch
 
-        self.DISTANCE_THRESHOLD = 145 #distance threshold for obstacles in mm
-        self.MOVE_POWER = 300/2 #70% power
-        self.TURN_POWER = 50 #50% power
+        self.FOLLOW_TIME = 10000        # wall following timer (ms)
+        self.DISTANCE_THRESHOLD = 200   # wander / follow threshold (mm)
+        self.DISTANCE_FOLLOW = 145      # wall following distance (mm)
+        self.MAX_POWER = 300
+        self.MOVE_POWER = 150           # 70% power?
+        self.TURN_POWER = 50            # 50% power?
+
+        self.current_state = State.WANDERING
 
 
     def stop(self):
@@ -40,26 +53,76 @@ class Explorer:
                 self.left_motor.run(self.MOVE_POWER)
                 self.right_motor.run(-self.MOVE_POWER)
 
+    # reset after hitting obstacle
+    def obstacle(self):
+        print("obstacle detected")
+        self.ev3.speaker.play_file(SoundFile.OKEY_DOKEY)
+        self.stop()
+        wait(500)
+        self.left_motor.run(-self.MOVE_POWER)
+        self.right_motor.run(-self.MOVE_POWER)
+        wait(500)
+        self.stop()
+        self.turn(90)
+        print("turning")
+
+    # sawtooth wander pattern __/|__/|__
+    def wander():
+        self.stop()
+        self.turn(45)
+        wait(500)
+        self.left_motor.run(self.MOVE_POWER)
+        self.right_motor.run(self.MOVE_POWER)
+        wait(2000)
+        self.stop()
+        self.turn(-135)
+
+    def wall_follow(self, wall_distance):
+        # simple error correction
+        error = wall_distance - self.DISTANCE_FOLLOW
+        correction = error * 2
+
+        # Ensure speed is (MIN < speed < MAX)
+        left_speed = max(min(self.MOVE_POWER + correction, self.MAX_SPEED), -self.MAX_SPEED)
+        right_speed = max(min(self.MOVE_POWER - correction, self.MAX_SPEED), -self.MAX_SPEED)
+        
+        # Adjust motors to maintain consistent wall distance
+        self.left_motor.run(left_speed)
+        self.right_motor.run(right_speed)
+
     def explore(self):
         print("hello")
+        self.stopwatch.reset()
         while True:
-            self.left_motor.run(self.MOVE_POWER)
-            self.right_motor.run(self.MOVE_POWER)
-            #if self.ultrasonic2.distance() < 50:
-            if self.touch.pressed():
-                print("obstacle detected")
-                self.ev3.speaker.play_file(SoundFile.OKEY_DOKEY)
-                self.stop()
-                wait(500)
-                self.left_motor.run(-self.MOVE_POWER)
-                self.right_motor.run(-self.MOVE_POWER)
-                wait(500)
-                self.stop()
-                self.turn(90)
-                print("turning")
-            #  if self.supersonic.distance() > self.DISTANCE_THRESHOLD:
-            #     self.turn(-90)
+            # goal reached?
             if self.colorsensor.color() == Color.RED:
                 self.stop()
                 self.ev3.speaker.play_file(SoundFile.OKEY_DOKEY)
-                return False    
+                return False
+
+            # move
+            self.left_motor.run(self.MOVE_POWER)
+            self.right_motor.run(self.MOVE_POWER)
+            
+            # State machine
+            # WALL_FOLLOW if touch or close to wall
+            if self.current_state == State.WANDERING:
+                if self.supersonic.distance() <= self.DISTANCE_THRESHOLD:
+                    self.current_state = State.WALL_FOLLOWING
+                    self.stopwatch.reset()
+                elif self.touch.pressed():
+                    self.current_state = State.WALL_FOLLOWING
+                    self.obstacle()
+                    self.stopwatch.reset()
+                else:
+                    self.wander()
+            # WANDER if NOT close to wall or follow timer expired
+            elif self.current_state == State.WALL_FOLLOWING:
+                wall_distance = self.supersonic.distance()
+                if wall_distance > self.DISTANCE_THRESHOLD:
+                    self.current_state = State.WANDERING
+                elif self.stopwatch.time() > self.FOLLOW_TIME:
+                    self.current_state = State.WANDERING
+                    self.wander()
+                else:
+                    self.wall_follow(wall_distance)
