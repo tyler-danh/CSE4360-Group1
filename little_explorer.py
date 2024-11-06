1d2be71f32aa6fe099ec5479e596883706dedb87
Daniel <105179611+275RR-Public@users.noreply.github.com>
2024-11-06 13:01:40 -0600

    Update little_explorer.py
    - fire fight smash pattern
    - 8 sec follow time
    - cleaned up goal logic
    - cleaned up State ENUM
    - various small bug fixes, clean ups

--- little_explorer.py
+++ little_explorer.py
@@ -2,14 +2,13 @@ from pybricks.parameters import Color, Stop
 from pybricks.media.ev3dev import SoundFile
 from pybricks.ev3devices import Motor, UltrasonicSensor, GyroSensor, ColorSensor, TouchSensor
 from pybricks.tools import wait
-# from enum import Enum, auto
+import sys
 
 # states of robot
-# auto() handles int assignment automatically
 class State:
     def _init_(self):
-        self.WANDERING = 1 #auto()
-        self.WALL_FOLLOWING = 2 #auto()
+        self.WANDERING = 1
+        self.WALL_FOLLOWING = 2
 
 class Explorer:
     def __init__(self, ev3, left_motor, right_motor,shovel_motor, gyroscope, ultrasonic, colorSensor, touch, watch):
@@ -22,30 +21,49 @@ class Explorer:
         self.touch = touch 
         self.stopwatch = watch
 
-        self.FOLLOW_TIME = 10000        # wall following timer (ms)
-        self.DISTANCE_THRESHOLD = 100  # wander / follow threshold (mm)
-        self.DISTANCE_FOLLOW = 60      # wall following distance (mm)
-        self.MAX_POWER = 300
-        self.MOVE_POWER = 300 
-        self.MOVE_POWER_1 = 150         # 70% power?
-        self.TURN_POWER = 50            # 50% power?
+        self.FOLLOW_TIME = 8000         # wall following timer (ms)
+        self.DISTANCE_THRESHOLD = 100   # wander / follow threshold (mm)
+        self.DISTANCE_FOLLOW = 60       # wall following distance (mm)
+        self.MAX_POWER = 300 
+        self.MOVE_POWER = 150
 
-        self.current_state = 1
-
+        self.current_state = State.WANDERING
 
     def stop(self):
         self.left_motor.stop(Stop.BRAKE)
         self.right_motor.stop(Stop.BRAKE)
 
-    def firefight(self):    
-        while True:
-        # print(shovel_motor.angle())
+    def smash_it(self):
+        for _ in range(3):
             self.shovel_motor.track_target(150)
             wait(500)
             self.shovel_motor.track_target(-130)
             wait(500)
-        return 
-   
+
+    def firefight(self):    
+        # initial smash
+        self.smash_it()
+        while True:
+            # if we get off goal then correct
+            # drive square pattern
+            if self.colorsensor.color() != Color.RED:
+                self.stop()
+                self.turn(90)
+                self.drive_forward(1000)
+
+            # fan smash pattern \|/
+            self.drive_forward(1000)
+            self.stop()
+            self.smash_it()
+
+            self.turn(45)
+            self.smash_it()
+
+            self.turn(-90)
+            self.smash_it()
+
+            self.turn(45)
+
     def turn(self, angle):
         print("turning: {:.2f}".format(angle))
         self.gyroscope.reset_angle(0)
@@ -53,45 +71,41 @@ class Explorer:
         if angle > 0:
             #print("left")
             while current_angle <= angle:
-                self.left_motor.run(-self.MOVE_POWER_1 )
-                self.right_motor.run(self.MOVE_POWER_1 )
+                self.left_motor.run(-self.MOVE_POWER )
+                self.right_motor.run(self.MOVE_POWER )
                 current_angle = self.gyroscope.angle()
                 #print(current_angle)
         else:
             #print("right")
             while current_angle >= angle:
-                self.left_motor.run(self.MOVE_POWER_1)
-                self.right_motor.run(-self.MOVE_POWER_1)
+                self.left_motor.run(self.MOVE_POWER)
+                self.right_motor.run(-self.MOVE_POWER)
                 current_angle = self.gyroscope.angle()
                 #print(current_angle)
         self.stop()
 
+    # move forward with goal and touch checks
     def drive_forward(self, drive_time):
         time = 0
         self.stopwatch.reset()
         while(time < drive_time):
-            if self.check_goal == False:
-                self.stop()
-                self.ev3.speaker.play_file(SoundFile.OKEY_DOKEY)
-                self.firefight()
-                return
-            self.left_motor.run(self.MOVE_POWER_1)
-            self.right_motor.run(self.MOVE_POWER_1)
+            self.check_goal()
+            self.left_motor.run(self.MOVE_POWER)
+            self.right_motor.run(self.MOVE_POWER)
             if self.touch.pressed():
-                self.current_state = 2
+                self.current_state = State.WALL_FOLLOWING
                 self.obstacle()
                 self.stopwatch.reset()
                 return
             time = self.stopwatch.time()
-        return
         
     # reset after hitting obstacle
     def obstacle(self):
         print("obstacle detected")
-        self.ev3.speaker.play_file(SoundFile.OKEY_DOKEY)
+        self.ev3.speaker.play_file(SoundFile.BACKING_ALERT)
         self.stop()
-        self.left_motor.run(-self.MOVE_POWER_1)
-        self.right_motor.run(-self.MOVE_POWER_1)
+        self.left_motor.run(-self.MOVE_POWER)
+        self.right_motor.run(-self.MOVE_POWER)
         wait(1000)
         self.stop()
         self.turn(80)
@@ -102,80 +116,75 @@ class Explorer:
         self.ev3.speaker.play_file(SoundFile.HORN_2)
         self.stop()
         self.turn(45)
-
         self.drive_forward(3500)
-
         self.stop()
         self.turn(-90)
-
         self.drive_forward(2500)
 
+    # wall follow at specific distance from wall
     def wall_follow(self, wall_distance):
-        # print("we are wall following")
-        # simple error correction
         self.ev3.speaker.play_file(SoundFile.HORN_1)
+        
+        # simple error correction
         error = wall_distance - self.DISTANCE_FOLLOW
         correction = error * 10
-
         # Ensure speed is (MIN < speed < MAX)
-        left_speed = max(min(self.MOVE_POWER_1 + correction, self.MAX_POWER), -self.MAX_POWER)  #+ 25
-        right_speed = max(min(self.MOVE_POWER_1 - correction, self.MAX_POWER), -self.MAX_POWER)
-        
+        left_speed = max(min(self.MOVE_POWER + correction, self.MAX_POWER), -self.MAX_POWER)  #+ 25
+        right_speed = max(min(self.MOVE_POWER - correction, self.MAX_POWER), -self.MAX_POWER)
         # Adjust motors to maintain consistent wall distance
         self.left_motor.run(left_speed)
         self.right_motor.run(right_speed)
 
+    # entry point - loop (goal, wander, wall follow)
     def explore(self):
-        print("hello")
         self.stopwatch.reset()
         while True:
             # goal reached?
-            if self.check_goal() == False:
-                self.stop()
-                self.ev3.speaker.play_file(SoundFile.OKEY_DOKEY)
-                self.firefight()
-                return False
+            self.check_goal()
 
             # move
-            self.left_motor.run(self.MOVE_POWER_1)
-            self.right_motor.run(self.MOVE_POWER_1)
+            self.left_motor.run(self.MOVE_POWER)
+            self.right_motor.run(self.MOVE_POWER)
             
             # State machine
             # WALL_FOLLOW if touch or close to wall
-            if self.current_state == 1:
-                # print("we are wandering")
+            if self.current_state == State.WANDERING:
                 if self.supersonic.distance() <= self.DISTANCE_THRESHOLD:
-                    self.current_state = 2
+                    self.current_state = State.WALL_FOLLOWING
                     self.stopwatch.reset()
                 elif self.touch.pressed():
-                    self.current_state = 2
+                    self.current_state = State.WALL_FOLLOWING
                     self.obstacle()
                     self.stopwatch.reset()
                 else:
                     self.wander()
             # WANDER if NOT close to wall or follow timer expired
-            elif self.current_state == 2:
+            elif self.current_state == State.WALL_FOLLOWING:
                 print("we are wall following")
                 wall_distance = self.supersonic.distance()
                 if wall_distance > self.DISTANCE_THRESHOLD:
                     print("we passed the wall")
-                # this turns us too the right when the wall ends and drives us forward
+                    # this turns us too the right when the wall ends
+                    # and drives us forward
                     self.turn(-90)
-                    self.drive_forward(3500)    #3500= 1 square                   
-                    self.current_state = 1
+                    self.drive_forward(3500) #3500 ~ 1 square
+                    self.turn(-90)                 
+                    self.current_state = State.WANDERING
                 elif self.touch.pressed():
-                    self.current_state = 2
+                    self.current_state = State.WALL_FOLLOWING
                     self.obstacle()
                     self.stopwatch.reset()
                 elif self.stopwatch.time() > self.FOLLOW_TIME:
-                    self.current_state = 1
+                    self.current_state = State.WANDERING
                     self.wander()
                 else:
                     self.wall_follow(wall_distance)
 
+    # goal reached so fight fire then exit
     def check_goal(self):
         if self.colorsensor.color() == Color.RED:
-            return False
-        else:
-            return True
-
+            print("goal")
+            self.stop()
+            self.ev3.speaker.play_file(SoundFile.CONFIRM)
+            self.firefight()
+            sys.exit()
