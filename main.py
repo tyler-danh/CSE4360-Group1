ffc3585ca12d96eb57d1357f73acac7600812580
tyler-danh <txd6321@mavs.uta.edu>
2024-11-27 15:51:26 -0600

    sumo functions written, needs testing

--- main.py
+++ main.py
@@ -10,103 +10,76 @@ from little_explorer import Explorer
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
+DISTANCETHRESHOLD = 1200 #approximate ring diameter
+
 # Init and beep when ready.
 ev3 = EV3Brick()
 ev3.speaker.play_file(SoundFile.MOTOR_START)
 
 watch = StopWatch()
 
-gyro = GyroSensor(Port.S2, Direction.COUNTERCLOCKWISE)
-print("gyro in")
-ultrasonic = UltrasonicSensor(Port.S1)
-print("ultrasonic in")
-colors = ColorSensor(Port.S3)
-print("color sensor in")
-#ultrasonic2 = UltrasonicSensor(Port.S4)
-touch = TouchSensor(Port.S4)
+gyro = GyroSensor(Port.S1, Direction.COUNTERCLOCKWISE)
 
-left_motor = Motor(Port.D, positive_direction=Direction.CLOCKWISE, gears=None)
-right_motor = Motor(Port.C, positive_direction=Direction.CLOCKWISE, gears=None)
-shovel_motor = Motor(Port.B, positive_direction=Direction.CLOCKWISE, gears=None)
+color_sensor = ColorSensor("""PORT HERE""") #PLEASE PUT IN PORT FOR COLOR SENSOR
 
-#gyro.reset_angle(0)
-# while ultrasonic.distance() > 150:
-#     # angle = gyro.angle()
-#     # if angle == 90 or angle == -90:
-#     #     gyro.reset_angle(0)
-#     #     print("reset angle")
-#     # print(angle)
+left_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE, gears=None)
+right_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE, gears=None)
 
-#     # color = colors.color()
-#     # if color != Color.BLACK:
-#     #     print(color)
-#     print(ultrasonic.distance())
-#     left_motor.dc(50)
-#     right_motor.dc(50)
+left_sonic = UltrasonicSensor("""PORT HERE""")
+right_sonic = UltrasonicSensor("""PORT HERE""") #REMEMBER TO PUT PORTS FOR ULTRASONIC
 
+#----------------------SUMO FUNCTIONS-----------------------------#
 
+def lockon():
+    while(left_sonic.distance() > DISTANCETHRESHOLD or right_sonic.distance() > DISTANCETHRESHOLD): #clockwise turn until target is found
+        left_motor.run(200)
+        right_motor.run(-200)
 
-# while True:
-#     print(shovel_motor.angle())
-#     intial_angle = shovel_motor.angle()
-#     shovel_motor.track_target(150)
-#     wait(500)
-#     shovel_motor.track_target(-100)
-#     wait(500)
-
-# while True:
-#     print(gyro.angle())
-
-""" 
-def stop():
-    left_motor.stop(Stop.BRAKE)
-    right_motor.stop(Stop.BRAKE) 
+    left_dist = left_sonic.distance()
+    right_dist = right_sonic.distance()
+    
+    if abs(left_dist - right_dist) < 60: #this means target is centered... hopefully
+        ev3.speaker.play_file(SoundFile.SHOUTING)
+        return True
+    elif left_dist < right_dist:
+        turn(-30)
+        return False
+    elif left_dist > right_dist:
+        turn(30)
+        return False
 
 def turn(angle):
-    print("turning: {:.2f}".format(angle))
     gyro.reset_angle(0)
     current_angle = 0
     if angle > 0:
-        #print("left")
         while current_angle <= angle:
+            left_motor.run(-200)
+            right_motor.run(200)
             current_angle = gyro.angle()
-            print(current_angle)
-            left_motor.run(-150 )
-            right_motor.run(150 )
     else:
-        #print("right")
         while current_angle >= angle:
-            print(current_angle)
-            left_motor.run(150)
-            right_motor.run(-150)
+            left_motor.run(200)
+            right_motor.run(-200)
             current_angle = gyro.angle()
 
-# sawtooth wander pattern __/|__/|__
-print("we are wandering")
-stop()
-turn(45)
+def stop():
+    left_motor.stop(Stop.BRAKE)
+    right_motor.stop(Stop.BRAKE)
 
-time = 0
-watch.reset()
-print("straight")
-while(time < 2000):
-    left_motor.run(150 )
-    right_motor.run(150 )
-    time = watch.time()
+def attack():
+    target_detected = False
+    while target_detected != True:
+        lockon()
+    ev3.speaker.play_file(SoundFile.T_REX_ROAR)
+    left_motor.dc(100) #SEND IT FORWARD!
+    right_motor.dc(100)
 
-stop()
-turn(-135)
+    if color_sensor.color() == Color.RED: #maybe change color detection for testing?
+        stop()
+        left_motor.dc(-60)
+        right_motor.dc(-60)
 
-time = 0
-watch.reset()
-print("straight")
-while(time < 1500):
-    left_motor.run(150)
-    right_motor.run(150)
-    time =watch.time()   
- """
+#----------------------SUMO FUNCTIONS----------------------------------#
 
 
-explorer = Explorer(ev3, left_motor, right_motor,shovel_motor, gyro, ultrasonic, colors, touch, watch)
-explorer.explore()
 ev3.speaker.play_file(SoundFile.HORN_1)
